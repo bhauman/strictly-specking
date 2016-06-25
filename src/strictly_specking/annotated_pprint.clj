dbe4e2e96e7283accf5aba4efc1a324701641bf7
Bruce Hauman <bhauman@gmail.com>
2016-06-25 13:50:06 -0400

    make annotations work for all collections

--- src/strictly_specking/annotated_pprint.clj
+++ src/strictly_specking/annotated_pprint.clj
@@ -48,11 +48,16 @@ This makes it easier to override pprint functionality for certain types."
      (pp/write-out obj))
     (pp/write-out obj)))
 
-(def orig-pprint-map (.getMethod pp/simple-dispatch clojure.lang.IPersistentMap))
+;; these methods are private so ... have to fetch them this way
+(def orig-pprint-map    (.getMethod pp/simple-dispatch clojure.lang.IPersistentMap))
+(def orig-pprint-vector (.getMethod pp/simple-dispatch clojure.lang.IPersistentVector))
+(def orig-pprint-list   (.getMethod pp/simple-dispatch clojure.lang.ISeq))
+(def orig-pprint-set    (.getMethod pp/simple-dispatch clojure.lang.IPersistentSet))
+
 
 ;; should create a dispatch that uses the normal method for maps
 ;; and this method for annotated maps
-(defn pprint-map-with-pointer [amap]
+(defn pprint-map-with-pointer* [amap]
   (let [comments (or (-> amap meta :comments) {})
         comments? (not-empty comments)]
     (pp/pprint-logical-block
@@ -86,6 +91,11 @@ This makes it easier to override pprint functionality for certain types."
           (when (next aseq)
             (recur (next aseq)))))))))
 
+(defn pprint-map-with-pointer [amap]
+  (if (-> amap meta :comments)
+    (pprint-map-with-pointer* amap)
+    (orig-pprint-map amap)))
+
 (defn abbrev-pprint-map-with-pointer [amap]
   (if-let [abr-level  (-> amap meta :abbrev)]
     (let [length (if (= :tight abr-level) 2 4)
@@ -100,6 +110,50 @@ This makes it easier to override pprint functionality for certain types."
   (defn pprint-comment-pointer [comm-point]
     (apply pprint-message (string/split (:text comm-point) #"\n"))))
 
+(defn pprint-seq-with-pointer [pre suf avec comment-key-fn]
+  (let [comments (or (-> avec meta :comments) {})
+        comments? (not-empty comments)]
+    ;;abbrev print-length can be 3 + max key
+    (pp/pprint-logical-block
+     :prefix pre :suffix suf
+     (pp/print-length-loop
+      [aseq (seq avec)
+       c    0]
+      (when aseq
+        (if-let [{:keys [comment value-colors comment-colors]
+                  :as current-comment} (get comments (comment-key-fn (first aseq) c))]
+          (do
+            (print-colored value-colors (first aseq))
+            (pp/pprint-newline :mandatory)
+            (print-colored comment-colors (CommentPointer. comment))
+            #_(pp/pprint-newline :mandatory))
+          (pp/write-out (first aseq)))
+        (when (next aseq)
+          (.write ^java.io.Writer *out* " ")
+          (pp/pprint-newline :linear)
+          (recur (next aseq) (inc c))))))))
+
+(defn pprint-vector-with-pointer [avec]
+  (if (-> avec meta :comments)
+    (pprint-seq-with-pointer "[" "]" avec (fn [_ c] c))
+    (orig-pprint-vector avec)))
+
+(defn pprint-set-with-pointer [a-set]
+  (if (-> a-set meta :comments)
+    (pprint-seq-with-pointer "#{" "}" a-set (fn [a _] a))
+    (orig-pprint-set a-set)))
+
+(defn pprint-simple-list-with-pointer [alis]
+  (pprint-seq-with-pointer "(" ")" alis (fn [_ c] c)))
+
+(defn pprint-list-with-pointer [aseq]
+  (if (-> aseq meta :comments)
+    (pprint-simple-list-with-pointer aseq)
+    (orig-pprint-list aseq)))
+
+(use-method error-path-dispatch clojure.lang.IPersistentSet pprint-set-with-pointer)
+(use-method error-path-dispatch clojure.lang.ISeq pprint-list-with-pointer)
+(use-method error-path-dispatch clojure.lang.IPersistentVector pprint-vector-with-pointer)
 (use-method error-path-dispatch clojure.lang.IPersistentMap abbrev-pprint-map-with-pointer)
 (use-method error-path-dispatch CommentPointer pprint-comment-pointer)
 
@@ -124,7 +178,11 @@ This makes it easier to override pprint functionality for certain types."
     (f data)))
 
 (defn annotate-path-only [data path annotation]
-  (path-only-data-structure data path #(vary-meta % merge annotation)))
+  (path-only-data-structure data path
+                            #(do
+                               #_(prn path %)
+                               (when (coll? %)
+                                 (vary-meta % merge annotation)))))
 
 (defn annotate-path [data path annotation]
   (update-in data path #(vary-meta % merge annotation)))
@@ -133,11 +191,32 @@ This makes it easier to override pprint functionality for certain types."
   (reduce #(annotate-path %1 (first %2) (second %2))
           data path-to-annotation-map))
 
+(ansi-util/with-ansi-when true
+    (pp/with-pprint-dispatch error-path-dispatch
+      (pp/pprint
+       (annotate-path-only test-data
+                           [:cljsbuild :builds 0 #_:source-paths-set]
+                           {:abbrev true
+                            :comments {:source-paths {
+                                          :key-colors [:green]
+                                          :value-colors [:red]
+                                          :comment-colors [:magenta]
+                                          :comment " is screwwed"
+                                          }
+                                       #_"tests" #_{
+                                          :key-colors [:green]
+                                          :value-colors [:red]
+                                          :comment-colors [:magenta]
+                                          :comment " is screwwed"
+                                          }}}))))
+
 (comment
   (def test-data
     {:cljsbuild {:assert true
                  :builds [{ :id "example-admin"
                            :source-paths ["src" "dev" "tests" "../support/src"]
+                           :source-paths-list '("src" "dev" "tests" "../support/src")
+                           :source-paths-set #{"src" "dev" "tests" "../support/src"}
                            :notify-command ["notify"]
                            :figwheel
                            { :websocket-host "localhost"
