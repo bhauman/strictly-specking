4995cd29ecb7eec7ed3297fda4de2887c269b739
Bruce Hauman <bhauman@gmail.com>
2016-07-09 12:02:30 -0400

    added intelligent message presentation

--- src/strictly_specking/context_print.clj
+++ src/strictly_specking/context_print.clj
@@ -1,12 +1,13 @@
 (ns strictly-specking.context-print
   (:require
    [strictly-specking.ansi-util :refer [color]]
+   [clojure.pprint :as pp]
    [clojure.java.io :as io]
    [clojure.string :as string]))
 
 ;; TODO make it general and possible to point to any structure on a path
 
-;; displaying error in context of file
+;; * Displaying a message in context of file
 
 (defn fetch-lines [file]
   (let [file (io/file file)]
@@ -20,12 +21,70 @@
 (defn blanks [n]
   (apply str (repeat n " ")))
 
+;; wrapping the message intellgently
+(defn determine-message-format [column msg]
+  (let [parts (string/split msg #"\n")
+        message-width (reduce max (map count parts))
+        marker-width 6
+        max-width 80]
+    ;; need to decide if it can go before the marker
+    (cond
+      (> (+ message-width marker-width) max-width)
+      ;; message on newline at start
+      (cons (format (str "%" column "s%s") "" "^---")
+              (map
+               #(format (str "%" message-width "s") %)
+               parts))
+      (> (+ message-width marker-width column) max-width)
+      (if (< (+ message-width 2) column)
+        ;; message before
+        (cons (format (str "%" (- column 2) "s  %s") (first parts) "^---" )
+              (map
+               #(format (str "%" (- column 2) "s") % )
+               (rest parts)))
+        ;; message newline centered
+        (cons (format (str "%" column "s%s") "" "^---")
+              (map
+               #(format (str "%" (int (/ (- max-width message-width) 2))
+                             "s%s") "" %)
+               parts)))
+      :else
+      ;; message after
+      (cons (format (str "%" column "s%s  %s") "" "^---" (first parts))
+            (map
+             #(format (str "%" (+ column marker-width) "s%s") "" %)
+             (rest parts))))))
+
+(comment
+  
+  (determine-message-format 20
+                            "asdf asdf asdf\nasdf asdf asdf asdf")
+
+  (determine-message-format 60
+                            "asdf asdf asdf\nasdf asdf asdf asdf")
+  
+  (determine-message-format
+   40
+   "Makes a function which can directly run format-in. The function is
+fn [& args] ... and returns nil. This version of the formatter macro is
+designed to be used with *out* set to an appropriate Writer. In particular,
+this is meant to be used as part of a pretty printer dispatch method.
+
+format-in can be either a control string or a previously compiled format.")
+  
+  (determine-message-format
+   40
+   "Makes a function which can directly run format-in.
+fn [& args] ... and returns nil. This version of
+designed to be used with *out* set to an appropriate Wri
+this is meant to be used as part of a 
+
+format-in can be either a control string or a p")
+  )
+
 (defn format-message [column msg]
-  (when-let [lines   (not-empty (string/split msg #"\n"))]
-    (->> (rest lines)
-         (map #(str (blanks (+ 6 column)) %))
-         (cons (str (blanks column) "^---  " (first lines) "\n"))
-         (mapv #(vector :message-line nil %)))))
+  (mapv #(vector :message-line nil %)
+        (determine-message-format column msg)))
 
 (defn insert-message [numbered-lines line column message]
   (concat
